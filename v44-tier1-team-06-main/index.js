c2aae4d74e5071ab14501601d39641ddb01fc647
elizabeth_roberts <elizabeth.roberts1927@gmail.com>
2023-06-01 17:56:53 -0300

    Manually updated code in HTML, JS and CSS from last merge on main.

--- v44-tier1-team-06-main/index.js
+++ v44-tier1-team-06-main/index.js
@@ -1,4 +1,4 @@
-// -------- GRID -------- //
+// GRID CONTAINER
 const container = document.querySelector(".container");
 
 let boardHeight;
@@ -16,6 +16,9 @@ const diceTwo = document.querySelector(".dice-two");
 const placeholders = document.querySelectorAll(".placeholder");
 const timerDiv = document.querySelector(".timer");
 const diceDiv = document.querySelector(".roll-button-dice-container");
+const noMatchMessage = document.querySelector(".no-match");
+let closeButton = document.getElementById("close-button");
+
 
 function createGrid() {
     // gives us the option to expand the board dimensions in two player mode
@@ -54,24 +57,28 @@ let clickedCells = [];
 // shade cells on the grid
 function shade(event) {
     let targetCell = event.target;
-    targetCell.classList.toggle("shaded");
-    clickedCells.push(targetCell);
+    if (targetCell.classList.contains("shaded")) {
+        targetCell.classList.remove("shaded");
+        // check if the cell is already in the clickedCells array
+        const index = clickedCells.indexOf(targetCell);
+        if (index > -1) {
+            // if it is, remove it from the array
+            clickedCells.splice(index, 1);
+        }
+    } else {
+        // if it's not in the array, add it to the clickedCells arry and shade the cell
+        targetCell.classList.add("shaded");
+        clickedCells.push(targetCell);
+    }
 }
 
 // use the clear button to unshade the cells that were just clicked (but not submitted) so the user can try a different combo of cells
 clearButton.addEventListener("click", () => {
     clickedCells.forEach((cell) => {
-        cell.classList.toggle("shaded");
+        cell.classList.remove("shaded");
     });
     clickedCells = [];
 });
-
-// CLEAR GRID FUNCTION
-function clearGrid() {
-    cells.forEach((cell) => {
-        cell.classList.remove("shaded");
-    });
-}
 
 // FULL GRID WIN CONDITION
 // create an array from the cells node list
@@ -86,11 +93,11 @@ const checkIfAllShaded = () => {
         newGame.classList.remove("hidden");
         messageDiv.classList.remove("hidden");
         winLoseMessage.innerText = "🎉 You win! 🥳";
-        clearButton.style.display = "none";
-        submitButton.style.display = "none";
-        diceRoller.style.display = "none";
-        diceOne.style.display = "none";
-        diceTwo.style.display = "none";
+        clearButton.classList.add("hidden");
+        submitButton.classList.add("hidden");
+        diceRoller.classList.add("hidden");
+        diceOne.classList.add("hidden");
+        diceTwo.classList.add("hidden");
         timerDiv.style.display = "none";
         // adds a point to total wins
         totalWins += 1;
@@ -98,6 +105,148 @@ const checkIfAllShaded = () => {
         winsFullGrid += 1;
     }
 }
+
+// DICE ROLLER
+diceRoller.addEventListener("click", handleRollButtonClick);
+let diceOneValue;
+let diceTwoValue;
+
+function handleRollButtonClick() {
+    // adjust CSS
+    diceDiv.style.justifyContent = "flex-start";
+    diceDiv.style.paddingLeft = "75px";
+
+    // when the roll button is clicked, generate random numbers between 1 and 6 for each of the die
+    diceOneValue = Math.floor(Math.random() * 6) + 1;
+    diceTwoValue = Math.floor(Math.random() * 6) + 1;
+
+    // set the src attribute of each dice image depending on the randomly generated number
+    let diceOneSrc = `img/dice${diceOneValue}.png`;
+    diceOne.setAttribute('src', diceOneSrc);
+    let diceTwoSrc = `img/dice${diceTwoValue}.png`;
+    diceTwo.setAttribute('src', diceTwoSrc);
+
+    // hide the placeholders when the die are rolled
+    placeholders.forEach(placeholder => placeholder.style.display = "none");
+
+    // ensures that there are no duplicate event listeners on the cells
+    cells.forEach((cell) => {
+        cell.removeEventListener("click", shade);
+    });
+
+    // only AFTER the roll button is clicked, shade a cell when clicked and push that cell to the clickedCells array
+    cells.forEach((cell) => {
+        cell.addEventListener("click", shade);
+    });
+}
+
+submitButton.addEventListener("click", handleSubmitButtonClick);
+
+function handleSubmitButtonClick() {
+    console.log("submission:");
+    const product = diceOneValue * diceTwoValue;
+    const clickedCellsCurrentTurn = [...clickedCells];
+
+    console.log(`product: ${product}`)
+    console.log(`length of clickedCells array: ${clickedCells.length}`);
+    console.log(`length of clickedCellsCurrentTurn array: ${clickedCellsCurrentTurn.length}`);
+
+    if (product !== clickedCellsCurrentTurn.length) {
+        console.log("doesn't match");
+        noMatchMessage.classList.remove("hidden");
+    } else {
+        console.log("match");
+        clickedCells = [];
+        checkIfAllShaded();
+    }
+}
+
+closeButton.addEventListener("click", function () {
+    noMatchMessage.classList.add("hidden"); 
+});
+
+
+//timer
+var timer = 60;
+var interval = setInterval(function() {
+    timer--;
+    $('.timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
+
+function resetTimer() {
+    timer=61;
+};
+
+// Leaderboard Section
+
+// lose condition of forfeiting two consecutive turns
+let forfeitTurnPoints = 0;
+// let count = 0; 
+// diceRoller.addEventListener("click", function () {
+//     count++;
+//     if (count === 2) {
+//         console.log("You lose!") 
+//         forfeitTurnPoints++;
+//     } 
+// });
+
+// const count = 0; this should have been a global function so outside of the event lister.
+
+// win condition of the whole grid being shaded.  Carol is working on this one I think?
+let fullGridPoints = 0; // This is just a placeholder for working on the icons and the point trackers.
+
+
+//  Icon section to see number of points when hovering over icon
+
+const forfeitIcon = document.getElementById("fa-solid fa-font-awesome small-icon");  // Ask if this is the right icon to use.
+
+forfeitIcon.addEventListener("mousemover", function () {
+    forfeitIcon.innerHTML = forfeitTurnPoints.length;
+}); 
+
+const fullGridIcon = document.getElementById("fa-solid fa-square small-icon");
+
+fullGridIcon.addEventListener("mouseover", function () {
+    fullGridIcon.innerHTML = filledGridWin.length
+});
+
+const totalIcon = document.getElementById("");
+
+totalIcon.addEventListener("mouseover", function () {
+    totalIcon.innerHTML // Ask about this one.  I might have to add an array like the other point trackers.
+})
+
+
+// Point tracking section
+
+const lossPointsTotal = function () {
+    const losePointsTracker = document.getElementById("losePointsTracker");
+    losePointsTracker.innerHTML = forfeitTurnPoints.length;
+}
+
+/*
+const winPointsTotal = function () {
+    const winPointsTracker = document.getElementById("winPointsTracker");
+    winPointsTracker.innerHTML = fullGridPoints.length;
+}
+*/
+
+const totalPoints = function () {
+    const totalPointsTracker = document.getElementById("totalPointsTracker")
+    totalPointsTracker.innerHTML = forfeitTurnPoints.length + fullGridPoints.length; // Not sure if this is correct.  Will have to ask.
+
+};
+
+
+
+// CLEAR GRID FUNCTION
+function clearGrid() {
+    cells.forEach((cell) => {
+        cell.classList.remove("shaded");
+    });
+}
+
 
 // -------- NEW GAME BUTTON -------- //
 const newGamebutton = document.querySelector(".button-new");  
@@ -115,57 +264,13 @@ resetTimer();
 clearGrid();
 }); 
 
-// -------- DICE ROLLER -------- //
-diceRoller.addEventListener("click", handleRollButtonClick);
 
-function handleRollButtonClick() {
-    // adjust CSS
-    diceDiv.style.justifyContent = "flex-start";
-    diceDiv.style.paddingLeft = "75px";
-
-    // when the roll button is clicked, generate random numbers between 1 and 6 for each of the die
-    let diceOneValue = Math.floor(Math.random() * 6) + 1;
-    let diceTwoValue = Math.floor(Math.random() * 6) + 1;
-
-    // set the src attribute of each dice image depending on the randomly generated number
-    let diceOneSrc = `img/dice${diceOneValue}.png`;
-    diceOne.setAttribute('src', diceOneSrc);
-
-    let diceTwoSrc = `img/dice${diceTwoValue}.png`;
-    diceTwo.setAttribute('src', diceTwoSrc);
-
-    // hide the placeholders when the die are rolled
-    placeholders.forEach(placeholder => placeholder.style.display = "none");
-
-    // only AFTER the roll button is clicked, shade a cell when clicked and push that cell to the clickedCells array
-    cells.forEach((cell) => {
-        cell.addEventListener("click", shade);
-    });
-
-    // submit button is only clickable AFTER the die are rolled
-    // remove the click event listener from every cell in the clickedCells array after the submit button is clicked
-    submitButton.addEventListener("click", function () { 
-        clickedCells.forEach((clickedCell) =>
-            clickedCell.removeEventListener("click", shade)
-        );
-        // empty out the array so the submitted cells don't get cleared if the clear button is clicked
-        clickedCells = [];
-        checkIfAllShaded();
-        count = 0;
-    });
+function timesUp() {
+    if (timer = 1) {
+        console.log("You lose")
+    }
 }
-
-// -------- TIMER -------- //
-var timer = 60;
-var interval = setInterval(function() {
-    timer--;
-    $('.timer').text(timer);
-    if (timer === 0) clearInterval(interval);
-}, 1000);
-
-function resetTimer() {
-    timer=60;
-};
+timesUp();
 
 // -------- LEADERBOARD -------- //
 // fetching icons
